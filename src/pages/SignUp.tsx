190eb47fac892041460f590e12df5632278cb41b
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-07-15 22:34:31 +0600

    signup working in backend but giving error in frontend

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,41 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSignupMutation } from "../redux/api/apiSlice";
 
 const SignUp = () => {
-  const signup = useSignupMutation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [signup, { isLoading }] = useSignupMutation();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const { email, password, confirmPassword } = e.target.elements;
 
     if (password !== confirmPassword) {
-      alert("Passwords don't match");
+      setErrorMessage("Passwords don't match");
       return;
     }
 
-    signup({ email, password });
+    try {
+      await signup({ email, password });
+      alert(`Account created for ${email}`);
+     
+    } catch (error) {
+      setErrorMessage("Error occurred during signup");
+    }
   };
 
   return (
-    <div className="mx-auto  my-8 card w-96 bg-white shadow-xl">
+    <div className="mx-auto my-8 card w-96 bg-white shadow-xl">
       <form onSubmit={handleSubmit} className="form-control my-8 items-center">
         <input
           type="email"
           placeholder="Email"
           className="input input-bordered input-secondary w-full max-w-xs my-4"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           className="input input-bordered input-secondary w-full max-w-xs my-4"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <input
           type="password"
           placeholder="Confirm password"
           className="input input-bordered input-secondary w-full my-4 max-w-xs"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
-        <button className="btn btn-primary mb-12" type="submit">
-          Sign up
+
+        {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
+        <button
+          className="btn btn-primary mb-12"
+          type="submit"
+          disabled={isLoading}
+        >
+          {isLoading ? "Signing up..." : "Sign up"}
         </button>
       </form>
     </div>
