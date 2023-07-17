7f0fa3fd18c4cb8ff9ed94f02dc8386eca41037f
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-07-18 02:17:17 +0600

    backend route added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://bookzone-k7nc.onrender.com/",
     prepareHeaders: (headers, state) => {
       const token = localStorage.getItem("token");
       const email = localStorage.getItem("email");
-     
+
       if (token) {
         headers.set("Authorization", `Bearer ${token}`);
       }
